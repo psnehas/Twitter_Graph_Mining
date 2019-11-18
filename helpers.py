aafd8c431851ad50ed940ad0ff6e648fba007b74
Arkil Thakkar <thakkararkil1996@gmail.com>
2019-11-18 12:37:57 -0800

    trying diff algo

---
+++ helpers.py
@@ -0,0 +1,35 @@
+
+import networkx as nx
+from seaborn import color_palette, set_style, palplot
+
+
+def create_color_map(G, attribute, seaborn_palette="colorblind"):
+    """Return a list of hex color mappings for node attributes"""
+    attributes = [G.node[label][attribute] for label in G.nodes()]
+
+    # get the set of possible attributes
+    attributes_unique = list(set(attributes))
+    num_values = len(attributes_unique)
+
+    # generate color palette from seaborn
+    palette = color_palette(seaborn_palette, num_values).as_hex()
+
+    # create a mapping of attribute to color
+    color_map = dict(zip(attributes_unique, palette))
+
+    # map the attribute for each node to the color it represents
+    node_colors = [color_map[attribute] for attribute in attributes]
+
+    return node_colors, color_map, palette
+
+def map_communities(G, communities):
+    """Return a mapping of community membership from a community set tuple"""
+
+    community_map = {}
+    for node in G.nodes():
+        for i, comm in enumerate(communities):
+            if node in comm:
+                community_map[node] = i
+        if community_map.get(node, None) is None:
+            community_map[node] = None
+    return community_map
